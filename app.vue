216f3b39450eed3d466173fbf90347900d3aa400
lrs <447380142@qq.com>
2024-07-25 19:29:13 +0800

    1.翻译下拉框增加属性，方便无障碍访问 2.页面增加lang属性 3.部分图片根据谷歌浏览器建议使用NuxtImage标签 4.

--- app.vue
+++ app.vue
@@ -7,36 +7,14 @@
 
 
 </template>
-<script>
-// export default {
-//   data() {
-//     return {
-//       isScrolling: false
-//     };
-//   },
-//   mounted() {
-//     if (process.client) {
-//       window.addEventListener('scroll', this.handleScroll);
-//     }
-//   },
-//   beforeDestroy() {
-//     if (process.client) {
-//       window.removeEventListener('scroll', this.handleScroll);
-//     }
-//   },
-//   methods: {
-//     handleScroll() {
-//       if (process.client && window.scrollY > 0) {
-//         this.isScrolling = true;
-//         console.log("scrolling");
-//       } else {
-//         this.isScrolling = false;
-//         console.log("not scrolling");
-//       }
-//     }
-//   }
-// };
+<script setup> //使用setup，下面是给html提供lang属性
+const { locale } = useI18n();
 
+useHead({
+  htmlAttrs: {
+    lang: locale.value
+  }
+});
 </script>
 
 <!-- 在这个特定的样式规则中，它使得链接在被选中时文字颜色变为# -->
--- components/header-nav.vue
+++ components/header-nav.vue
@@ -14,7 +14,7 @@
 		</ul>
 		<!-- 移动端 -->
 		<ul class="nav-m" :class="{'collapse':!isCollapse}">
-			<img src="assets/images/animal-pilates.png" alt="logo" class="logo-img">
+			<NuxtImg src="assets/images/animal-pilates.png" alt="logo" class="logo-img" />
 			<li>
 				<router-link :to="localPath('/')" @click="isCollapse = false" >ALITA</router-link>
 			</li>
@@ -25,7 +25,7 @@
 				<router-link :to="localPath('/products')" @click="isCollapse = false" >{{ $t('products_intro') }}</router-link>
 			</li>
 		</ul>
-		<button @click="isCollapse = !isCollapse">
+		<button @click="isCollapse = !isCollapse" aria-label="Toggle Navigation">
 			<svg width="30px" height="30px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)rotate(0)" stroke="#ffffff" stroke-width="0.36"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#ffffff" stroke-width="1.152"> <path d="M22 18.0048C22 18.5544 21.5544 19 21.0048 19H12.9952C12.4456 19 12 18.5544 12 18.0048C12 17.4552 12.4456 17.0096 12.9952 17.0096H21.0048C21.5544 17.0096 22 17.4552 22 18.0048Z" fill="#973d01"></path> <path d="M22 12.0002C22 12.5499 21.5544 12.9954 21.0048 12.9954H2.99519C2.44556 12.9954 2 12.5499 2 12.0002C2 11.4506 2.44556 11.0051 2.99519 11.0051H21.0048C21.5544 11.0051 22 11.4506 22 12.0002Z" fill="#973d01"></path> <path d="M21.0048 6.99039C21.5544 6.99039 22 6.54482 22 5.99519C22 5.44556 21.5544 5 21.0048 5H8.99519C8.44556 5 8 5.44556 8 5.99519C8 6.54482 8.44556 6.99039 8.99519 6.99039H21.0048Z" fill="#973d01"></path> </g><g id="SVGRepo_iconCarrier"> <path d="M22 18.0048C22 18.5544 21.5544 19 21.0048 19H12.9952C12.4456 19 12 18.5544 12 18.0048C12 17.4552 12.4456 17.0096 12.9952 17.0096H21.0048C21.5544 17.0096 22 17.4552 22 18.0048Z" fill="#973d01"></path> <path d="M22 12.0002C22 12.5499 21.5544 12.9954 21.0048 12.9954H2.99519C2.44556 12.9954 2 12.5499 2 12.0002C2 11.4506 2.44556 11.0051 2.99519 11.0051H21.0048C21.5544 11.0051 22 11.4506 22 12.0002Z" fill="#973d01"></path> <path d="M21.0048 6.99039C21.5544 6.99039 22 6.54482 22 5.99519C22 5.44556 21.5544 5 21.0048 5H8.99519C8.44556 5 8 5.44556 8 5.99519C8 6.54482 8.44556 6.99039 8.99519 6.99039H21.0048Z" fill="#973d01"></path> </g></svg>
 		</button>
 	</nav>
